70a469d7e19115327853b3e55b4689d8bbbaf245
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-13 19:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  var stringOutput = 'Happy holidays, ${name}!'
+  var stringOutput = `Happy holidays, ${name}!`
   return stringOutput
 }
 function happyHolidayTo(holiday, name){
-  var stringOutput = 'Happy ${holiday}, ${name}!'
+  var stringOutput = `Happy ${holiday}, ${name}!`
   return stringOutput
 }
 function holidayCountdown(holiday, days){
